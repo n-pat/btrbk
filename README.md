231c0521aeb76beddc64cbe65781451a6137b6cf
Patrick Neumann <patrick@neuma-nn.de>
2019-05-30 19:53:35 +0200

    Buildscript for docker containers

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # btrbk
-multi-arch docker container for btrbk based on alpine
+
+WORK IN PROGRESS
+
+## Version 0.1
+
+Multi-arch docker container for btrbk based on alpine
+
+## Architectures
+
+(Template taken from: https://github.com/docker-library/official-images#architectures-other-than-amd64)
+
+* Architectures officially supported by Docker, Inc. for running Docker: (see download.docker.com)
+	* ARMv7 32-bit (arm32v7): https://hub.docker.com/u/arm32v7/
+	* ARMv8 64-bit (arm64v8): https://hub.docker.com/u/arm64v8/
+	* Linux x86-64 (amd64): https://hub.docker.com/u/amd64/
+* Other architectures built by official images: (but not officially supported by Docker, Inc.)
+	* ARMv5 32-bit (arm32v5): https://hub.docker.com/u/arm32v5/
+	
---
+++ build.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+VERSION="$1"
+product_name="n-pat/btrbk"
+
+# parameter: "platform-architecture"
+function build_and_push_images() {
+  local arch=$1
+
+  docker build -t "$product_name:$os-$arch-$VERSION" - <<<EOF -
+		FROM $arch/alpine:latest
+
+		#WORKDIR ["/"]
+
+		#RUN apk --no-cache add btrbk
+		RUN apk --update upgrade \
+			&& apk --no-cache --no-progress add \
+				btrbk \
+				tzdata \
+			&& rm -rf /var/cache/apk/*
+
+		ENTRYPOINT ["btrbk"]
+		CMD ["dryrun"]
+		EOF
+
+  for os in alpine; do
+    docker tag  "$product_name:$os-$arch-$VERSION" "$product_name:$os-$arch"
+    #docker push "$product_name:$os-$arch-$VERSION"
+    #docker push "$product_name:$os-$arch"
+  done
+}
+
+function build_all() {
+  for tag in $@; do
+    build_and_push_images "$tag"; fi
+  done
+  docker rmi $(docker images -q -f dangling=true)
+}
+
+#build_all 'amd64 arm32v6 arm32v7 arm32v8'
+#build_all 'x86_64 arm32v7'
+build_all 'amd64 arm32v7'
+exit 0
